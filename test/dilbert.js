d3452b3b38b687f5a7aa73422d1e7c1c96fb6307
David Aaron Suddjian <aasuddjian@gmail.com>
2016-10-18 04:28:49 -0700

    tests

---
+++ test/dilbert.js
@@ -0,0 +1,24 @@
+var expect = require('chai').expect
+var request = require('request')
+
+var goodOlDilby = require('../dilbert')
+
+describe('dilbert', function() {
+  for (var i = 0; i < 20; i++) {
+    it('should fetch a comic ' + (i + 1) + ' times', function(done) {
+      this.timeout(10000)
+      goodOlDilby(function(err, data) {
+        expect(err).to.not.exist
+        expect(data).to.exist
+          .and.have.all.keys('date', 'url')
+        expect(data.date).to.match(/\d{4}-\d\d?-\d\d?/)
+        request(data.url, function(err, response, body) {
+          expect(err).to.not.exist
+          expect(response).to.exist
+            .and.to.have.property('statusCode', 200)
+          done()
+        })
+      })
+    })
+  }
+})
