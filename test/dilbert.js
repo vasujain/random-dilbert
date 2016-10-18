39504f8ce1249c56b7f44f2461679873cb881271
David Aaron Suddjian <aasuddjian@gmail.com>
2016-10-18 13:43:05 -0700

    test to make sure urls are different

--- test/dilbert.js
+++ test/dilbert.js
@@ -4,13 +4,16 @@ var request = require('request')
 var goodOlDilby = require('../dilbert')
 
 describe('dilbert', function() {
-  for (var i = 0; i < 20; i++) {
+  var cachedURLs = []
+  for (var i = 0; i < 50; i++) {
     it('should fetch a comic ' + (i + 1) + ' times', function(done) {
       this.timeout(10000)
       goodOlDilby(function(err, data) {
         expect(err).to.not.exist
         expect(data).to.exist
           .and.have.all.keys('date', 'url')
+        expect(data.url).to.not.be.oneOf(cachedURLs)
+        cachedURLs.push(data.url)
         expect(data.date).to.match(/\d{4}-\d\d?-\d\d?/)
         request(data.url, function(err, response, body) {
           expect(err).to.not.exist
